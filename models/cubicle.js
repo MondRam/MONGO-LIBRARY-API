90b1fcb309d0f34d709f6c4056eab14a5cf95860
MondRam <utm22030544@utma.edu.mx>
2024-06-13 12:43:07 -0600

    feat: the file cubicle.js inside the folder models

---
+++ models/cubicle.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const CubicleSchema = new mongoose.Schema({
+    número: {type: Number, required: true,},
+    asigando: {type: Boolean, required: true},
+    usuario: {type: String,},
+
+}, {timestamps: true});
+
+const CubicleModel = mongoose.model('Cubicle', CubicleSchema);
+
+export default CubicleModel;
